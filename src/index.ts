2887db46790904894180e712b0eff1c8062e58a4
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-08-09 21:42:49 +0500

    cmit

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,12 @@
-// // import http from 'http';
-// // import WebSocket, { WebSocketServer } from 'ws';
+// import http from 'http';
+// import WebSocket, { WebSocketServer } from 'ws';
 
-// // const server = http.createServer(function(request: any, response: any) {
-// //     console.log((new Date()) + ' Received request for ' + request.url);
-// //     response.end("hi there");
-// // });
+// const server = http.createServer(function(request: any, response: any) {
+//     console.log((new Date()) + ' Received request for ' + request.url);
+//     response.end("hi there");
+// });
 
-// // const wss = new WebSocketServer({ server });
-
-// // wss.on('connection', function connection(ws) {
-// //   ws.on('error', console.error);
-
-// //   ws.on('message', function message(data, isBinary) {
-// //     wss.clients.forEach(function each(client) {
-// //       if (client.readyState === WebSocket.OPEN) {
-// //         client.send(data, { binary: isBinary });
-// //       }
-// //     });
-// //   });
-
-// //   ws.send('Hello! Message From Server!!');
-// // });
-
-// // server.listen(8080, function() {
-// //     console.log((new Date()) + ' Server is listening on port 8080');
-// // });
-
-// import express from 'express';
-// import { WebSocket, WebSocketServer } from 'ws';
-
-// const app = express()
-// const httpServer = app.listen(8080)
-
-// const wss = new WebSocketServer({ server: httpServer });
+// const wss = new WebSocketServer({ server });
 
 // wss.on('connection', function connection(ws) {
 //   ws.on('error', console.error);
@@ -47,3 +21,29 @@
 
 //   ws.send('Hello! Message From Server!!');
 // });
+
+// server.listen(8080, function() {
+//     console.log((new Date()) + ' Server is listening on port 8080');
+// });
+
+import express from 'express';
+import { WebSocket, WebSocketServer } from 'ws';
+
+const app = express()
+const httpServer = app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.send('Hello! Message From Server!!');
+});
